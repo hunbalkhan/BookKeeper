66724e6e9321d170e49509b24588afc9f9bfa2d9
hunbalkhan <hunbal4@gmail.com>
2025-10-10 12:12:38 -0400

    Initial files created, and main steps/roadmap added

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,23 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world!");
+    }
+
+    // --------- Make CSV File ---------
+
+
+    // --------- The Home Menu ---------
+
+
+    // --------- The Ledger Screen ---------
+
+
+    // --------- The Reports Screen ---------
+
+
+    // --------- Polish up and clean code where it can be cleaned ---------
+}
